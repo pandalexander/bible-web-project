69a9c91306fc2392f01f36a9edba618068612a2d
Alex Seeley <alexseeley134@gmail.com>
2024-10-15 16:43:02 -0600

    Add tailwind and project font

---
+++ src/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center w-screen">
+        <h1>Home Screen w/ Tailwind!</h1>
+      </div>
+    </>
+  );
+}
